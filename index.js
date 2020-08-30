1ee8648a140c96dbd99f31860e31d849630e73d9
lucas <lucas.laborne@gmail.com>
2020-08-30 06:40:23 -0300

    'ok'

--- index.js
+++ index.js
@@ -1,94 +1,85 @@
 const axios = require('axios').default;
 const fs = require('fs');
 
-const QUERY_URL = 'https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exintro&format=json&explaintext';
+const DATAFILE = 'data.json';
+const URL_REDDIT = 'http://www.reddit.com/r/random.json';
 
-let data;
+function load(file = DATAFILE) {
+    if (fs.existsSync(file))
+        return JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
+    return {
+        pages: 0,
+        total: 0,
+    };
+}
+let result = load();
 
-function load() {
-    if (!fs.existsSync('data.json')) {
-        fs.writeFileSync('data.json', JSON.stringify(
-            { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "paginas": 0, "lastTitle": "", "total": 0 }
-        ));
-    }
-    data = JSON.parse(fs.readFileSync('data.json', { encoding: 'utf-8' }));
+function save(file = DATAFILE, data = result) {
+    fs.writeFileSync(file, JSON.stringify(data), { encoding: 'utf-8' });
 }
 
-function save() {
-    fs.writeFileSync('data.json', JSON.stringify(data));
-}
+async function reddit() {
+    let batch = [];
+    try {
+        const payload = (await axios.get(URL_REDDIT)).data;
+        const posts = payload.data.children;
+        result.pages++;
+        for (let { data } of posts) {
+            // console.log(data.title);
+            // batch.push(data.title);
 
-function mostrarResultados() {
-    console.log('\n#' + data.paginas);
-    let total = 0;
-    for (let i = 0; i <= 9; i++) {
-        if (data[i])
-            total += data[i];
-    }
-    console.log(`Digito\tOcorr.\t%`);
-    for (let i = 0; i <= 9; i++) {
-        if (data[i]) {
-            const perc = ((data[i] / total) * 100);
-            console.log(`${i}: \t${data[i]} \t${perc.toFixed(2)}%`);
+            // console.log(data.selftext);
+            // batch.push(data.selftext);
+
+            // console.log(data.num_comments);
+            batch.push(data.num_comments);
         }
+    } catch (ex) {
+        console.error(ex);
     }
-    console.log('Total: ' + total);
-    console.log('');
+    return batch;
+}
+
+function extractNumbers(textArr) {
+    let batch = [];
+    for (let txt of textArr) {
+        if (!txt) continue;
+        txt = 1 * txt;
+        txt = txt.toString();
+        let num = txt
+            .replace(/(\r\n|\n|\r)/gm, '');
+        if (txt) batch.push(num);
+    }
+    return batch;
+}
+
+function display(numbers) {
+    if (!numbers || !numbers.length) return;
+    for (let n of numbers) {
+        if (!n) continue;
+        let first = n.toString()[0];
+        if (!result[first]) result[first] = 1;
+        else result[first]++;
+        result.total++;
+    }
+    if (!result.total) return;
     save();
-}
-
-async function farm() {
-    while (true) {
-        let resp = await axios.get(QUERY_URL);
-        data.paginas++;
-        let payload = resp.data;
-
-        let pages = payload.query.pages;
-        let page = pages[Object.keys(pages)[0]]
-        let content = page.extract;
-        let currtitle = page.title;
-
-        let parts = content.split(' ');
-        for (let i = 0; i < parts.length; i++) {
-            let p = parts[i];
-            p = p.replace('-', ' ')
-            p = p.replace('–', ' ')
-            p = p.replace(/[^\w\s]/gi, '');
-            p = p.toLowerCase();
-            p = p.trim();
-
-
-
-            if (!p) continue;
-            if (!isNaN(p)) {
-                let frase = '';
-                for (let j = i - 5; j < i + 5; j++) {
-                    if (j < 0) continue;
-                    if (j >= parts.length) break;
-                    frase += parts[j] + ' ';
-                }
-
-                console.clear();
-                console.log(`Titulo: ${currtitle}\n`);
-                console.log(`Frase: ${frase}\n`);
-                if (p[0] === '0')
-                    p *= 1;
-                if (!p) continue;
-                let firstDigit = p[0];
-                console.log('Numero: ' + p);
-
-                data.total += 1;
-                if (data[firstDigit])
-                    data[firstDigit]++;
-                else
-                    data[firstDigit] = 1;
-                mostrarResultados();
-            }
-        }
-
-
+    console.clear();
+    console.log(`Pages: ${result.pages}`);
+    console.log(`Digit \tOccur.\t%`);
+    for (let i = 1; i <= 9; i++) {
+        if (!result[i]) continue;
+        let digit = i;
+        let occ = result[i];
+        let perc = (occ / result.total) * 100;
+        console.log(`${digit}\t${occ}\t${perc.toFixed(2)}`);
     }
+    console.log(`Total: \t${result.total}`);
+    return;
 }
 
-load();
-farm();
+(async function main() {
+    while (true) {
+        display(extractNumbers([...await reddit()]));
+    }
+})();
--- teste.js
+++
@@ -1,4 +0,0 @@
-const axios = require('axios');
-const URL = 'https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exintro&format=json&explaintext';
-
-
